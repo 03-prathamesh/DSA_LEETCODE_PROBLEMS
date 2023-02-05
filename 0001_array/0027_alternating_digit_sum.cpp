5e2f15a32bf10cd0c08411d2698acc042f0f241d
prathamesh <prathameshamundkar@gmail.com>
2023-02-05 21:31:04 +0530

    adding the problem

--- 0001_array/0027_alternating_digit_sum.cpp
+++ 0001_array/0027_alternating_digit_sum.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int alternateDigitSum(int n) {
 
-        vector<int>ans;
-        
-      
+            vector<int>ans;
+            vector<int>temp;
 
-          vector<int>temp;
              while(n>0){
 
                   int digit=n%10;
@@ -21,8 +19,8 @@ public:
                  ans.push_back(temp[i]);
              }
         
-int sum=0;
-int sm=0;
+             int sum=0;
+             int sm=0;
 
        for(int i=0;i<ans.size();i=i+2){
             
