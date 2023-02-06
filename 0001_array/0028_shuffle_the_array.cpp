23731285ef9a202ebc9c79cbc33a956157e87651
prathamesh <prathameshamundkar@gmail.com>
2023-02-06 16:12:34 +0530

    adding file

--- 0001_array/0028_shuffle_the_array.cpp
+++ 0001_array/0028_shuffle_the_array.cpp
@@ -33,3 +33,25 @@ public:
          
     }
 };
+
+
+//learning from others solutions
+
+class Solution {
+public:
+
+    vector<int> shuffle(vector<int>& nums, int n) {
+        
+        vector<int>res;
+
+        int j = n;
+
+        for(int i = 0; i < n; i++)
+        {
+           res.push_back(nums[i]);
+           res.push_back(nums[j++]);
+        }
+
+        return res;
+    }
+};
