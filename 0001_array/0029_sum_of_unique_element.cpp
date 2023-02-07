f3be720b75a1918a13b4d5de4310855a22a52d13
prathamesh <prathameshamundkar@gmail.com>
2023-02-07 13:54:49 +0530

    adding another problem

---
+++ 0001_array/0029_sum_of_unique_element.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+  vector<int>unique_ele;
+
+   for(int i=0;i<nums.size();i++){
+
+       int count=0;
+
+       for(int j=0;j<nums.size();j++){
+
+            if(nums[i]==nums[j]){
+
+                count++;
+            }
+       }
+
+       if(count==1){
+
+           unique_ele.push_back(nums[i]);
+       }
+   }
+
+
+   
+
+   int sum=0;
+   for(int i=0;i<unique_ele.size();i++){
+
+       sum=sum+unique_ele[i];
+   }
+
+return sum;
+    }
+};
