e8d6d0f392b4c35e03f09b7b09ff499d0644776f
prathamesh <prathameshamundkar@gmail.com>
2023-02-07 23:19:18 +0530

    adding the efficient solution

--- 0001_array/0029_sum_of_unique_element.cpp
+++ 0001_array/0029_sum_of_unique_element.cpp
@@ -34,3 +34,19 @@ public:
 return sum;
     }
 };
+
+// efficient solution
+// TC: O(n)
+// SC: O(1)
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum = 0;
+        for(auto x: nums)
+        {
+            if(count(nums.begin(), nums.end(), x)==1)
+               sum+=x;
+        }
+        return sum;
+    }
+};
