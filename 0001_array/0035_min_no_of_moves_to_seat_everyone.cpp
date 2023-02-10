b58af82d6270fef6a0236eac017d80f6de0d47f6
prathamesh <prathameshamundkar@gmail.com>
2023-02-10 14:07:08 +0530

    leetcode/array

---
+++ 0001_array/0035_min_no_of_moves_to_seat_everyone.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+
+    sort(seats.begin(),seats.end());
+    sort(students.begin(),students.end());
+    int moves=0;
+
+   for(int i=0;i<seats.size();i++){
+      
+
+
+         if(students[i]>seats[i]){
+             moves=moves+(students[i]-seats[i]);
+         }
+         else{
+
+              moves=moves+(seats[i]-students[i]);
+         }
+
+       }
+
+      return moves;   
+           
+ 
+
+
+    }
+};
