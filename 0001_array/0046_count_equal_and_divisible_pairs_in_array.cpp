51431c478421014999770b69e3fe339c4acc4b06
prathamesh <prathameshamundkar@gmail.com>
2023-02-15 16:35:14 +0530

    array/leetcode

---
+++ 0001_array/0046_count_equal_and_divisible_pairs_in_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        
+
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+
+            for(int j=i+1;j<nums.size();j++){
+
+
+                int div=(i*j);
+               if(nums[i]==nums[j] && div%k==0){
+
+                    count++;
+               }
+
+
+            }
+        }
+
+        return count;
+    }
+};
