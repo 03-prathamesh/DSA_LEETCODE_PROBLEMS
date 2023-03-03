8a920b96fefd2da484d30f535893e186b7bb760e
prathamesh <prathameshamundkar@gmail.com>
2023-03-03 23:11:02 +0530

    learning how the hashmaps works

---
+++ 0001_array/0082_sort_the_peoples.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        
+  map<int,string>mp;
+ for(int i=0;i<heights.size();i++)
+{
+
+    mp[heights[i]]=names[i];
+
+ }
+// map stores the keys in the sorted order
+  vector<string>ans;
+
+  for( auto x: mp){
+
+      ans.push_back(x.second);
+  }
+  
+    reverse(ans.begin(),ans.end());
+
+    return ans;
+    
+    }
+};
