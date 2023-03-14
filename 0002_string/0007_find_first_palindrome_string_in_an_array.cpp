10996dc11a4ceebc5c813c46940bd3d9d7eb55d1
prathamesh <prathameshamundkar@gmail.com>
2023-03-15 01:55:32 +0530

    class/leetcode

--- 0002_string/0007_find_first_palindrome_string_in_an_array.cpp
+++ 0002_string/0007_find_first_palindrome_string_in_an_array.cpp
@@ -1,4 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
+   
    bool isPalindrome(string str){
           int i=0;
           int j=str.size()-1;
@@ -14,17 +18,32 @@ class Solution {
           return false;
    }
    public:
+  
     string firstPalindrome(vector<string>& words) {
          
-    vector<string>ans;
-   for(int i=0;i<words.size();i++){
-         if(isPalindrome(words[i])==true){
-            ans.push_back(words[i]);
-         }
+        vector<string>ans;
+        for(int i=0;i<words.size();i++){
+            if(isPalindrome(words[i])==true){
+                ans.push_back(words[i]);
+            }
+        }
+        if(ans.size()>0){
+        return ans[0];
+        }
+        return "";
     }
- if(ans.size()>0){
-     return ans[0];
- }
- return "";
-    }
+ 
+    //  void display(){
+    //    cout<<"the palindrome is : "<<firstPalindrome(vector<string>& words);
+    //  }
 };
+
+int main(){
+
+   Solution sp;
+   vector<string>answ= {"abc","car","ada","racecar","cool"};
+   string found= sp.firstPalindrome(answ);
+   cout<<"the palindrome is: "<<found;
+
+    return 0;
+}
