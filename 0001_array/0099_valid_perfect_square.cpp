9b0fab5514d57cdd2509cd47858cd851569763c8
prathamesh <prathameshamundkar@gmail.com>
2023-04-06 19:45:53 +0530

    using long long datatype

---
+++ 0001_array/0099_valid_perfect_square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long i;
+        for( i=1;i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
+};
