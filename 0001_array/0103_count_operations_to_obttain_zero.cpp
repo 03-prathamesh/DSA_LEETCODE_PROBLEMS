a9abade2f9b1d79637af25d4913ae84ab39db64a
prathamesh <prathameshamundkar@gmail.com>
2023-04-13 22:17:36 +0530

    maths/leetcode

---
+++ 0001_array/0103_count_operations_to_obttain_zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ans=0;
+        int tp1=num1;
+        int tp2=num2;
+
+        while(tp1!=0 && tp2!=0){
+            if(tp1>=tp2){
+               tp1=tp1-tp2;
+               ans++;
+            }
+            else{
+                 tp2=tp2-tp1;
+                 ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
