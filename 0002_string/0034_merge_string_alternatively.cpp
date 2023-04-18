e6c125f8b68afb74bd0a806a6479a10f124d13a0
prathamesh <prathameshamundkar@gmail.com>
2023-04-18 22:16:45 +0530

    changing something

--- 0002_string/0034_merge_string_alternatively.cpp
+++ 0002_string/0034_merge_string_alternatively.cpp
@@ -22,3 +22,32 @@ public:
         return ans;
     }
 };
+
+/* 
+lass Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int n=word1.size();
+        int m=word2.size();
+        int greater=max(n,m);
+        int i=0;
+        int j=0;
+        while(i<n || j<m){
+            if(i<n){
+                ans=ans+word1[i++];
+            }
+            if(j<m){
+                ans=ans+word2[j++];
+            }
+           
+        }
+        // for(int p=i;p<n;p++){
+        //     ans=ans+word1[p];
+        // }
+        // for(int p=j;p<m;p++){
+        //     ans=ans+word2[p];
+        // }
+        return ans;
+    }
+};*/
