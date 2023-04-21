1d7320cb3629605ca42079aab3dcd0fb9ca7c7f1
prathamesh <prathameshamundkar@gmail.com>
2023-04-21 15:15:44 +0530

    used the logic of find() function effectively

---
+++ 0002_string/0037_count_the_number_of_consistent_strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int ans=0;
+        for(auto x:words){
+                int found=0;
+            for(int i=0;i<x.size();i++){
+                for(int j=0;j<allowed.size();j++){
+                    if(x[i]==allowed[j]){
+                        found++;
+                        break;
+                    }
+                }
+                if(found==x.size()){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
