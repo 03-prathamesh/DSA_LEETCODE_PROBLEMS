014beda666afd8296889fcc4be65fa594dceeecb
prathamesh <prathameshamundkar@gmail.com>
2023-05-05 21:13:59 +0530

    array/leetcode

---
+++ 0001_array/0106_sum_multiples.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        vector<int>ans;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                ans.push_back(i);
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            sum+=ans[i];
+        }
+        return sum;
+    }
+};
