d44ad8ec705f15534c365a4dda5fadf1683ed623
prathamesh <prathameshamundkar@gmail.com>
2023-05-10 22:10:21 +0530

    string/leetcode

---
+++ 0002_string/0050_divide_a_string_into_group_of_size_k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> divideString(string s, int k, char fill) {
+        vector<string>ans;
+        string temp="";
+        int div=s.size()%k;
+        int t=0;
+        for(int i=0;i<s.size();i++){
+            if(t==k){
+                
+                t=0;
+                ans.push_back(temp);
+                temp.clear(); //temp="";
+            }
+            temp=temp+s[i];
+            t++;
+        }
+  
+        if(div>0){
+            for(int i=0;i<k-div;i++){
+                temp=temp+fill;
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+
+    }
+};
