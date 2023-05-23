7413fb550c1f04586a1145c7b5b7bf3523210f2c
prathamesh <prathameshamundkar@gmail.com>
2023-05-23 23:24:21 +0530

    dont know why this code giving me the error

---
+++ 0002_string/0052_valid_palindrome.cpp
@@ -0,0 +1,46 @@
+// class Solution {
+  
+// public:
+
+//    bool cap(char s){
+//        if((s>='A' && s<='Z')|| (s>='a' && s<='z') || (s>='0' && s<='9')) {
+//            return true;
+//        }
+//        return false;
+//     }
+
+//     bool isPalindrome(string s) {
+        
+//  string temp="";
+//  for(int i=0;i<s.size();i++){
+//      if(cap(s[i])){
+          
+//           char tp=tolower(s[i]);
+//           temp.push_back(tp);
+
+//      }
+//  }
+
+
+   
+//    int sta=0;
+//    int end=temp.size()-1;
+
+//    while(sta<=end){
+//        if(temp[sta]!=temp[end]){
+//            return false;
+//        }
+//        else{
+//          sta++;
+//          end--;
+//        }
+//    }
+//    return true;
+// //     string temp1=temp;
+// //    reverse(temp.begin(),temp.end());
+// //     if(temp==temp1){
+// //         return true;
+// //     }
+// //     return false;
+//     }
+// };  dont know why this code giving me the error evven if it is correct
