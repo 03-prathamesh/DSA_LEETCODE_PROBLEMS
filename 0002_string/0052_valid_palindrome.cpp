dac1c1dd70cd64a6a545ee342313c97b45eb3141
prathamesh <prathameshamundkar@gmail.com>
2023-05-24 21:46:39 +0530

    string/leetcode

--- 0002_string/0052_valid_palindrome.cpp
+++ 0002_string/0052_valid_palindrome.cpp
@@ -44,3 +44,22 @@
 // //     return false;
 //     }
 // };  dont know why this code giving me the error evven if it is correct
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for(int i=0;i<s.size();i++){
+            if(isupper(s[i])) {
+                s[i] = tolower(s[i]);
+            }
+
+            if(isalnum(s[i])) {
+                str.push_back(s[i]);
+            }
+        }
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        return str == rev;
+    }
+};
