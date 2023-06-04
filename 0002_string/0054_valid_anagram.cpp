b56353c70a02ea066b3c01161c6e4a01b355ed46
prathamesh <prathameshamundkar@gmail.com>
2023-06-04 13:01:39 +0530

    optimised solution

--- 0002_string/0054_valid_anagram.cpp
+++ 0002_string/0054_valid_anagram.cpp
@@ -17,3 +17,19 @@ public:
         return true;
     }
 };
+// optimised solution
+unordered_map<char,int>s1;
+        unordered_map<char,int>t1;
+        for(auto x:s){
+            s1[x]++;
+        }
+
+         for(auto x:t){
+            t1[x]++;
+        }
+
+        if(s1.size()!=t1.size()){
+            return false;
+        }
+
+        return s1==t1;
