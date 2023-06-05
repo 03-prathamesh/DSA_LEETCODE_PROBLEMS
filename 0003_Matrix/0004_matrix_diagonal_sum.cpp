8a1649b9ae2f6bbb3a1b6d4e95f489ca748e8de9
prathamesh <prathameshamundkar@gmail.com>
2023-06-05 17:22:55 +0530

    learning all about matrix

---
+++ 0003_Matrix/0004_matrix_diagonal_sum.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0;
+        int n=mat.size();
+        for(int i=0;i<mat.size();i++){
+               sum=sum+ mat[i][i];
+               sum=sum+ mat[i][n-1-i];     
+        }
+
+        if(n%2!=0){
+            int mid=n/2;
+            sum=sum-mat[mid][mid];
+        }
+        
+        return sum;
+    }
+};
