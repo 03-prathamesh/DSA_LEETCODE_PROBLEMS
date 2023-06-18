1b269d59563904bb7ac8fa40ea0395195c238234
prathamesh <prathameshamundkar@gmail.com>
2023-06-18 10:33:02 +0530

    erase function how to use and its  functinality

---
+++ 0002_string/0058_remove_trailing_zeroes_from_a_string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        
+        for(int i=num.size()-1;i>=0;i--){
+           
+            if(num[i]=='0'){
+                // num.erase(num.begin()+i);
+                num.erase(i,1);
+            }
+            else{
+                break;
+            }
+        }
+       
+        return num;
+    }
+};
