6907e5d059c730c4b7204ddcf23efe74c9536cc9
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-11-18 10:17:43 +0530

    used digonal changed approach here

---
+++ 0002_string/0066_find_unique_binary_string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int s=nums.size();
+        string ans="";
+        for(int i=0;i<nums.size();i++){
+            if(nums[i][i]=='0'){
+                ans=ans+'1';
+            }
+            else{
+                ans=ans+'0';
+            }
+        }
+        return ans;
+    }
+};
