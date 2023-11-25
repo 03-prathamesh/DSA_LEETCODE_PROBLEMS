63b0d358e7761b57074c7978caeba85bc069443d
prathamesh amundkar <prathameshamundkar@gmail.com>
2023-11-25 15:37:23 +0530

    solved with own logic without seeing solution

---
+++ 0001_array/0148_arithmetic_subarrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool check(vector<int>temp){
+        sort(temp.begin(),temp.end());
+        if(temp.size()<2){
+            return true;
+        }
+        int diff=temp[1]-temp[0];
+        for(int i=1;i<temp.size()-1;i++){
+            if((temp[i+1]-temp[i])!=diff){
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool>ans;
+        for(int i=0;i<l.size();i++){
+            vector<int>temp;
+            for(int j=l[i];j<=r[i];j++){
+               temp.push_back(nums[j]);
+            }
+             bool main_ans=check(temp);
+             ans.push_back(main_ans);
+             temp.clear();
+        }
+        return ans;
+    }
+};
