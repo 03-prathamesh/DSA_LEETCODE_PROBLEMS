c11eb194b1f174a2ebf693b8ddd17f972521ff8f
prathamesh amundkar <prathameshamundkar@gmail.com>
2024-01-08 10:29:35 +0530

    learned about the lower_bount and vector.back() function

---
+++ 0001_array/0159_longest_increasing_subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+
+       vector<int>ans;
+       ans.push_back(nums[0]);
+       for(int i=1;i<nums.size();i++){
+           if(nums[i]>ans.back()){
+               ans.push_back(nums[i]);
+           }
+           else{
+               auto it=lower_bound(ans.begin(),ans.end(),nums[i]);
+               *it=nums[i];
+           }
+       }
+
+
+
+          return ans.size();
+
+
+        }
+};
+
+
+
