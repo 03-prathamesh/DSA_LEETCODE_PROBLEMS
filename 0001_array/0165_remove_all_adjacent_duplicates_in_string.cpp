a381bff89c75aa660a06ffe8bea8a312f95940fd
prathamesh amundkar <prathameshamundkar@gmail.com>
2024-01-20 10:56:26 +0530

    learned new syntacx v.back().first and second

--- 0001_array/0165_remove_all_adjacent_duplicates_in_string.cpp
+++ 0001_array/0165_remove_all_adjacent_duplicates_in_string.cpp
@@ -46,6 +46,7 @@ public:
         }
     }
        
+       
     return ans;
     }
 };
---
+++ 0001_array/0166_v_back.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+v.back().first;
+v.back().second;
+
+
+search on chat gpt
+
+
+    return 0;
+}
