483cccf98bc138a313c0fc63e55c075c2d4e01b2
prathamesh amundkar <prathameshamundkar@gmail.com>
2024-02-26 11:47:26 +0530

    solved after long time after my laptop get repaired and learned alot from this question

---
+++ 0001_array/0174_array_with_element_not_equal_to_average_of_neighbours.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        for(int i=1;i<nums.size()-1;i++){
+
+            if((nums[i+1]>nums[i] && nums[i-1]<nums[i]) || (nums[i+1]<nums[i] && nums[i-1]>nums[i])){
+                swap(nums[i],nums[i+1]);
+            }
+        }
+        return nums;
+    }
+};
